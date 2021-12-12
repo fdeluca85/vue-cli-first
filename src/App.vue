693f2e224a628f80bf671b3ead28a8c986e587a7
francesca <fradeluca85@gmail.com>
2021-12-12 16:01:46 +0100

    title

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <h1>La mia prima app con Vue CLI!</h1>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<style scoped>
+h1{
+    color:#41B883 ;
+    text-transform: uppercase;
+}
+</style>
